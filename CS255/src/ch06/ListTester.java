d414b9f9ded9a493aed71c573baf351790d4379f
Steve Cardone <cardonesc@yahoo.com>
2018-03-25 15:45:52 -0500

    3/25/2018  Imported  chapter 06 files and began lab1.

---
+++ CS255/src/ch06/ListTester.java
@@ -0,0 +1,29 @@
+package ch06;
+
+import ch06.lists.ArraySortedList;
+import ch06.lists.ArrayUnsortedList;
+
+public class ListTester {
+
+	public static void main(String[] args) {
+		
+		//ArrayUnsortedList<String> test = new ArrayUnsortedList<String>();
+		ArraySortedList<String> test = new ArraySortedList<String>();
+		
+		test.add("two");
+		test.add("one");
+		test.add("two");
+		test.add("three");
+		test.add("four");
+		test.add("five");
+		test.add("two");
+		test.add("two");
+		
+		System.out.println("This many " + test.howMany("two"));
+		System.out.println(test.toString());
+		test.removeAll("two");
+		System.out.println(test.toString());
+
+	}
+
+}
